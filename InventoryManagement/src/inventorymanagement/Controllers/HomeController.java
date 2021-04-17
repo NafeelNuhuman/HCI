7585ecc917e8c6ec75c2b5199f1f6663358b1186
Nafeel <65773614+NafeelNuhuman@users.noreply.github.com>
2021-04-17 21:51:59 +0530

    Almost done with Home page

--- InventoryManagement/src/inventorymanagement/Controllers/HomeController.java
+++ InventoryManagement/src/inventorymanagement/Controllers/HomeController.java
@@ -5,20 +5,32 @@
  */
 package inventorymanagement.Controllers;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
  *
  * @author User
  */
+
+
 public class HomeController implements Initializable {
 
     /**
      * Initializes the controller class.
      */
+    @FXML 
+    private ListView<String> groupList = new ListView<String>();
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
